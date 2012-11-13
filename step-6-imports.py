222c31b9a3f510e61550c6f13f1a04977c91d5ea
jeffrhodeisland <mail@davidjeffrey.co.uk>
2012-11-13 16:08:27 +0000

    Added import / namespace presentation

---
+++ step-6-imports.py
@@ -0,0 +1,71 @@
+# Python North East
+# 'import' and namespacing
+
+# In Python we have some built-in functions and variables:
+
+print('This function is always available!')
+
+# We can see what is locally available using 'dir'
+print dir()
+
+airspeed_velocity_unladed_swallow = '24mph'
+# ref: http://style.org/unladedswallow/
+
+print dir()
+
+# The majority of things need to be 'imported'
+
+# 'os' is one of the standard modules available (Python 2.6+) 
+import os
+
+# Now, we have an object in our local namespace called 'os'
+print os
+
+# Notice that this isn't like PHP or Ruby where require/include pulls things
+# directly into the local namespace. Modules are always run 'once', unlike
+# PHP's 'include'. It's a bit like Java's import though.
+
+# This is a module object, and it can contain functions, variables etc.,
+print os.getcwd()
+
+# We can also pull individual pieces out of a module into our local namespace
+from os import getcwd
+# This is now local!!!
+getcwd()
+
+# You can import EVERYTHING from a module like this:
+from os import *
+
+# But try and avoid this as it fills your local namespace :)
+print dir()
+
+# This makes me a SAD panda
+# ...honestly, don't do this unless you have a good reason.
+
+# More examples:
+from datetime import date
+
+print date.today()
+
+# Packages are groupings of modules. Sub-modules are separated with a '.' 
+
+import xml.sax.saxutils
+
+# Note the full namespace is 'xml.sax.saxutils'
+print xml.sax.saxutils.escape('<p>Some nasty html</p')
+
+# We can import a submodule:
+from xml.sax import saxutils
+print saxutils.escape('<h1>More nasty HTML</h1>')
+
+# And objects directly from the submodule too
+from xml.sax.saxutils import escape
+print escape('<p>You get the idea...</p>')
+
+# Modules are actually just files or folders
+
+# Now to wrap up and hammer home that point about from <blah> import *
+import webbrowser
+webbrowser.open('http://www.youtube.com/watch?v=I6vPRaIrvqU')
+
+# Questions?
